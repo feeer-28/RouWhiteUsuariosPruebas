bee23a79ae7a6de04990ecd4fc5748094d878415
yesid <yesidortiz17@gmail.com>
2025-06-23 21:02:30 -0500

    cambiaso

---
+++ backend/models/usuarios.js
@@ -0,0 +1,60 @@
+'use strict';
+const { Model } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+    static associate(models) {
+      //asociar
+    }
+  }
+
+  Usuario.init({
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    correo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    contraseña: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    telefono: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            is: /^[0-9]+$/i ,// Validación solo numbers
+            is: /^\d{10}$/ // validacion 10 numbers
+
+        }
+    },
+    rol: {
+      type: DataTypes.ENUM('admin', 'despachador', 'conductor', 'usuario'),
+      defaultValue: 'usuario',
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Usuario',
+    tableName: 'Usuarios',
+    hooks: {
+      beforeCreate: async (usuario) => {
+        usuario.contraseña = await bcrypt.hash(usuario.contraseña, 10);
+      },
+      beforeUpdate: async (usuario) => {
+        if (usuario.changed('contraseña')) {
+          usuario.contraseña = await bcrypt.hash(usuario.contraseña, 10);
+        }
+      }
+    }
+  });
+
+  return Usuario;
+};
--- backend/routes/rutas.js
+++
@@ -1,8 +0,0 @@
-// routes/rutas.js
-const express = require('express');
-const router = express.Router();
-const rutasController = require('../controllers/rutasController');
-
-router.get('/', rutasController.obtenerRutas);
-
-module.exports = router;
